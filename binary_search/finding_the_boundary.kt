b4b37d9ecd67c08bce2122fafa4e6806886dbc93
duongdt <doanthanhduong11031990@gmail.com>
2021-10-19 14:19:18 +0700

    add findingTheBoundary

---
+++ binary_search/finding_the_boundary.kt
@@ -0,0 +1,43 @@
+package tinyflutterteam.binary_search
+
+
+fun findingTheBoundary(flags: BooleanArray): Int {
+    var indexOfFindNumber = -1
+    var indexLeft = 0
+    var indexRight = flags.size
+    while(indexLeft <= indexRight) {
+        val indexMid = (indexLeft + indexRight) / 2
+        val valueMid = flags[indexMid]
+        if(valueMid == false) {
+            indexLeft = indexMid + 1
+        } else {
+            indexRight = indexMid - 1
+            indexOfFindNumber = indexMid
+        }
+    }
+
+    println("FindingTheBoundary flags = ${flags.joinToString(",")}")
+    println("FindingTheBoundary index = $indexOfFindNumber")
+    return indexOfFindNumber
+}
+
+fun assert(result: Boolean) {
+    if(!result) {
+        throw Exception("assert fail $result")
+    }
+}
+
+fun main() {
+    val flags1: BooleanArray = booleanArrayOf(false, false, false, false, true)
+    val index1 = findingTheBoundary(flags1)
+    assert(index1 == 4)
+
+    val flags2: BooleanArray = booleanArrayOf(false, true, true, true)
+    val index2 = findingTheBoundary(flags2)
+    assert(index2 == 1)
+
+    val flags3: BooleanArray = booleanArrayOf(true, true, true, true, true, true , true, true, true )
+    val index3 = findingTheBoundary(flags3)
+    assert(index3 == 0)
+
+}
