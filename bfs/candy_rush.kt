75e168fa7ac506022d2b27d5a67dd5a88dbe9423
duongdt <doanthanhduong11031990@gmail.com>
2021-10-22 08:59:09 +0700

    candy rush done

--- bfs/candy_rush.kt
+++ bfs/candy_rush.kt
@@ -1,3 +1,6 @@
+import java.util.Queue
+import java.util.LinkedList
+
 fun main() {
     
     val board1: Array<IntArray> = arrayOf(
@@ -23,74 +26,118 @@ fun main() {
         intArrayOf(1, 5, 3, 3, 5),
         intArrayOf(1, 2, 3, 3, 4)
     )
+    val sizeBoard = 5
+    val maxIndex = sizeBoard - 1
+    showBoard(board2, "Board", sizeBoard)
+
+    for (row in 0..maxIndex) {
+        markWinCellsAtRow(board2, row, board2Copy, sizeBoard)
+    }
+
+    for (col in 0..maxIndex) {
+        markWinCellsAtCol(board2, col, board2Copy, sizeBoard)
+    }
+
+    showBoard(board2Copy, "Board after clear win cells", sizeBoard)
+
+    for (col in 0..maxIndex) {
+        rearrangeColAfterWin(board2Copy, col, sizeBoard)
+    }
+
+    showBoard(board2Copy, "Board after rearrange", sizeBoard)
     
-    showBoard(board2, "Board")
-
-    for (row in 0..4) {
-        var leftIndex: Int = 0
-        var rightIndex: Int = 1
-        
-        do {
-            
-            val startIndex = leftIndex
-            var endIndex = rightIndex
-            var isEndChainSame = false
-            if(board2[row][leftIndex] != board2[row][rightIndex]) {
-                leftIndex = rightIndex
-                isEndChainSame = true
-                endIndex--
-            } else if(rightIndex >= 4) {
-                isEndChainSame = true
-            }
-
-            val distanceSame = endIndex - startIndex + 1
-
-            if(distanceSame >= 3 && isEndChainSame) {
-                for (i in startIndex..endIndex) {
-                    board2Copy[row][i] = 0
-                }
-            }
-
-            rightIndex ++
-        } while(rightIndex < 5)
-    }
-
-    for (col in 0..4) {
-        var topIndex: Int = 0
-        var bottomIndex: Int = 1
-        
-        do {
-            
-            val startIndex = topIndex
-            var endIndex = bottomIndex
-            var isEndChainSame = false
-            if(board2[topIndex][col] != board2[bottomIndex][col]) {
-                topIndex = bottomIndex
-                isEndChainSame = true
-                endIndex--
-            } else if(bottomIndex >= 4) {
-                isEndChainSame = true
-            }
-
-            val distanceSame = endIndex - startIndex + 1
-
-            if(distanceSame >= 3 && isEndChainSame) {
-                for (i in startIndex..endIndex) {
-                    board2Copy[i][col] = 0
-                }
-            }
-
-            bottomIndex ++
-        } while(bottomIndex < 5)
-    }
-
-    showBoard(board2Copy, "Board after clear win cells")
 }
 
-fun showBoard(board: Array<IntArray>, boardName: String) {
+
+fun rearrangeColAfterWin(boardResult: Array<IntArray>, colIndex: Int, sizeBoard: Int,) {
+    var climbToTopIndex: Int = sizeBoard - 1
+    val zeroQueue: Queue<Int> = LinkedList<Int>()
+    
+    do {
+        val top = boardResult[climbToTopIndex][colIndex]
+        if(top == 0) {
+            zeroQueue.add(climbToTopIndex)
+        } else {
+            val zeroBottomIndex = zeroQueue.poll()
+            if(zeroBottomIndex != null) {
+                boardResult[zeroBottomIndex][colIndex] = top
+                boardResult[climbToTopIndex][colIndex] = 0
+                zeroQueue.add(climbToTopIndex)
+            }
+        }
+        climbToTopIndex --
+    } while(climbToTopIndex >= 0)
+}
+
+fun markWinCellsAtCol(board: Array<IntArray>, colIndex: Int, boardResult: Array<IntArray>, sizeBoard: Int,) {
+    val maxIndex = sizeBoard - 1
+    var topIndex: Int = 0
+    var bottomIndex: Int = 1
+    
+    while(bottomIndex < maxIndex) {
+        bottomIndex ++
+
+        val startIndex = topIndex
+        var endIndex = bottomIndex
+        var isEndChainSame = false
+        if(board[topIndex][colIndex] != board[bottomIndex][colIndex]) {
+            topIndex = bottomIndex
+            isEndChainSame = true
+            endIndex--
+        } else if(bottomIndex >= maxIndex) {
+            isEndChainSame = true
+        }
+
+        val distanceSame = endIndex - startIndex + 1
+
+        if(distanceSame >= 3 && isEndChainSame) {
+            for (i in startIndex..endIndex) {
+                boardResult[i][colIndex] = 0
+            }
+        }
+
+        
+    }
+}
+
+fun markWinCellsAtRow(
+    board: Array<IntArray>, 
+    rowIndex: Int, 
+    boardResult: Array<IntArray>, 
+    sizeBoard: Int,
+    ) {
+    val maxIndex = sizeBoard - 1
+    var leftIndex: Int = 0
+    var rightIndex: Int = 0
+    
+    while(rightIndex < maxIndex) {
+        rightIndex ++
+        val startIndex = leftIndex
+        var endIndex = rightIndex
+        var isEndChainSame = false
+
+        if(board[rowIndex][leftIndex] != board[rowIndex][rightIndex]) {
+            leftIndex = rightIndex
+            isEndChainSame = true
+            endIndex--
+        } else if(rightIndex >= maxIndex) {
+            isEndChainSame = true
+        }
+
+        val distanceSame = endIndex - startIndex + 1
+
+        if(distanceSame >= 3 && isEndChainSame) {
+            for (i in startIndex..endIndex) {
+                boardResult[rowIndex][i] = 0
+            }
+        }        
+    }
+}
+
+fun showBoard(board: Array<IntArray>, boardName: String, boardSize: Int) {
     println("$boardName")
-    for (row in 0..4) {
-        for (col in 0..4) {
+    for (row in 0..boardSize-1) {
+        for (col in 0..boardSize-1) {
             val cell = board[row][col]
             print("$cell  ")
         }
